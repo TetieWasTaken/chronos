947622be0e362aeab15b4f9cf9c54b7edfd6b1b9
TetieWasTaken <tjvssr@gmail.com>
2024-11-15 21:30:45 +0100

    fix(*): lint

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -67,7 +67,7 @@ export default function CalendarPage() {
 
       setAuthorising(false);
     });
-  }, []);
+  }, [router]);
 
   // Week handling
   const today = new Date();
@@ -147,7 +147,7 @@ export default function CalendarPage() {
         // router.push("/auth");
       }
     }
-  }, [calendar, currentWeekStart]);
+  }, [calendar, currentWeekStart, currentWeekEnd, authorising]);
 
   //  Add a new schedule item to the weekly schedule
   const handleAddScheduleItem = async () => {
@@ -298,7 +298,7 @@ export default function CalendarPage() {
               className="mb-3"
               styles={{
                 singleValue: (base) => ({ ...base, color: "white" }),
-                control: (base, _state) => ({
+                control: (base) => ({
                   ...base,
                   background: "#374151",
                 }),
